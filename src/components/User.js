e510c730f477ba8be2df8815974604f9fcb152b5
ChiuYuan0214 <rumble88481@gmail.com>
2022-01-31 14:03:53 +0800

    initial commit

---
+++ src/components/User.js
@@ -0,0 +1,19 @@
+import classes from './User.module.css';
+import React, { Component } from 'react';
+
+class User extends Component {
+
+  componentWillUnmount() {
+    console.log('User unmount!');
+  }
+
+  render() {
+    return <li className={classes.user}>{this.props.name}</li>;
+  }
+}
+
+// const User = (props) => {
+//   return <li className={classes.user}>{props.name}</li>;
+// };
+
+export default User;
